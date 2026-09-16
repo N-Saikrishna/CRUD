2395f959575a976a31df2a670d0b49cdfb9e061f
devpleg <devd4312@gmail.com>
2026-09-15 22:22:02 -0400

    adding contact into openapi yaml

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -4,14 +4,15 @@ require_once __DIR__ . '/common.php';
 
 $in = json_decode(file_get_contents('php://input'), true);
 
-function sendJson($obj)
+function sendJson($obj, $status = 200)
 {
+    http_response_code($status);
     header('Content-Type: application/json');
     echo json_encode($obj);
     exit();
 }
 
-foreach (array('id', 'userId') as $field) {
+foreach (array('id') as $field) {
     if (!isset($in[$field])) {
         sendJson(array("error" => "Missing required field: " . $field));
     }
@@ -33,7 +34,7 @@ $stmt->bind_param("ii", $id, $userId);
 $stmt->execute();
 
 if ($stmt->affected_rows === 0) {
-    sendJson(array("error" => "Contact not found"));
+    sendJson(array("error" => "Contact not found"), 404);
 }
 
 $stmt->close();
